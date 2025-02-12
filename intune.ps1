7a9f012ce9d4d0f7bdec3a781a76d2f3d5c1e8d3
Andy Kemp <40425562+andrew-kemp@users.noreply.github.com>
2025-02-12 20:09:05 +0000

    Update intune.ps1

--- intune.ps1
+++ intune.ps1
@@ -1,4 +1,4 @@
-$url = "https://aka.ms/intunewin"
+$url = "https://github.com/andrew-kemp/public/raw/refs/heads/main/IntuneWindowsAgent.msi"
 $output = "C:\Windows\Temp\IntuneManagementExtension.msi"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
